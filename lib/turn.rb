fa78204136d7fc06700b29eb784b639e392f22b5
Iris Quintero <godessiris@gmail.com>
2019-05-19 16:16:13 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,6 +13,16 @@ end
 
 def position_taken?(board, index)
   if board[index] == "X" || board[index] == "O"
-    TRUE
-  elsif board[index] == " "
+   true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil 
+  end
 end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    true 
+  else 
+    false 
+   end 
+end
+
